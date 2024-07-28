d4f583ca0838a1ccb2d011ed64da4aef7610b9cd
farshid <sputnick01.farshid75@gmail.com>
2024-07-28 18:08:54 +0330

    applay NavigationBar , BottomNavigation View, DrawerLayout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ android {
 dependencies {
 
     implementation(libs.androidx.core.ktx)
+    implementation(libs.smooth.bottom.bar)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.navigation.fragment.ktx)
--- app/src/main/java/com/example/kotlincourse/MainActivity.kt
+++ app/src/main/java/com/example/kotlincourse/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.kotlincourse
 
 
 import android.os.Bundle
+import android.widget.PopupMenu
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.example.kotlincourse.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +18,24 @@ lateinit var binding:ActivityMainBinding
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setUpNavigationComponent()
 
+    }
+
+    private fun setUpNavigationComponent() {
+    val navHostFragment:NavHostFragment= supportFragmentManager.findFragmentById(R.id.navHost) as NavHostFragment
+        val navController=navHostFragment.navController
+
+
+        val appBarConfiguration=
+            AppBarConfiguration(setOf(R.layout.fragment_home,R.layout.fragment_home),binding.drawerlayout)
+
+        binding.navView.setupWithNavController(navController)
+
+
+        val popupMenu=PopupMenu(this,null)
+        popupMenu.inflate(R.menu.menu_bottom)
+        val menu=popupMenu.menu
+        binding.bottomBar.setupWithNavController(menu,navController)
     }
 }
