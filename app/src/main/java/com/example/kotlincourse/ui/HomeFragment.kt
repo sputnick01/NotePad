3acbe7c369b7428419e494138eedb0ff8fd750ea
farshid <sputnick01.farshid75@gmail.com>
2024-07-28 14:41:41 +0330

    applay viewBinding

---
+++ app/src/main/java/com/example/kotlincourse/ui/HomeFragment.kt
@@ -0,0 +1,25 @@
+package com.example.kotlincourse.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.kotlincourse.R
+import com.example.kotlincourse.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+
+
+    lateinit var binding: FragmentHomeBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
